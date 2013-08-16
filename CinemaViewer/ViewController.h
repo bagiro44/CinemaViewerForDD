075325196dd7c725564fc8e24c6114a18f179f40
bagiro44 <dmitriyremezov@MacBook-Air-Dmitriy.local>
2013-08-16 10:25:45 +0400

    Initial Commit

---
+++ CinemaViewer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CinemaViewer
+//
+//  Created by Dmitriy Remezov on 16.08.13.
+//  Copyright (c) 2013 Dmitriy Remezov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
